8ecdf18b247c26a6ee69cf2769bcbcbb9536e3c0
yacoubi mohamed amine <aminecnam@gmail.com>
2024-07-29 20:16:02 -0400

    yaakoubi med amine hello dojo

---
+++ React/Intro_react/first_test/firstreactapp/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1> Hello Dojo! </h1>
+      <ul>This i need to Do :
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+
+    </>
+  );
+}
+
+export default App;
